b987fd693625a9eadc0877dd8ee9c236290f77e2
bvelychko <bogdan.velichko.work@gmail.com>
2021-12-01 02:40:23 +0200

    page object adding

--- src/test/java/selenide/hometask/one/hometask/RegistrationFlowStraightforward.java
+++ src/test/java/selenide/hometask/one/hometask/RegistrationFlowStraightforward.java
@@ -3,19 +3,26 @@ package selenide.hometask.one.hometask;
 import com.codeborne.selenide.Condition;
 import org.junit.Test;
 import org.openqa.selenium.By;
-
+import java.util.UUID;
 import static com.codeborne.selenide.Selenide.*;
 
-public class RegistrationAndLoginFlow {
+
+public class RegistrationFlowStraightforward {
+    String uuid= UUID.randomUUID().toString();
+    String email = uuid+"@gmail.com";
+
     @Test
     public void registration() {
+//  Open the registration form and wait till it loaded
         open("http://automationpractice.com/index.php");
         $(By.cssSelector("a[href*=my-account]")).click();
         sleep(3000);
+//  Click the "Create" button with empty form
         $(By.name("SubmitCreate")).click();
         sleep(3000);
         $(By.id("create_account_error")).shouldBe(Condition.textCaseSensitive("Invalid email address."));
-        $(By.name("email_create")).setValue("test1234567@gmail.com");
+//  Fill the form with valid data
+        $(By.name("email_create")).setValue(email);
         $(By.name("SubmitCreate")).scrollIntoView("true");
         $(By.name("SubmitCreate")).click();
         $(By.id("id_gender1")).click();
@@ -42,28 +49,26 @@ public class RegistrationAndLoginFlow {
         $(By.id("phone_mobile")).setValue("+312345678");
         $(By.id("alias")).clear();
         $(By.id("alias")).setValue("text");
+//  Submit the form with valid data
         $(By.name("submitAccount")).click();
         sleep(3000);
         $(By.className("info-account")).shouldBe(Condition.text("Welcome to your account. Here you can manage all of your personal information and orders."));
+//  Log out from the account
+        $(By.className("logout")).click();
+        sleep(3000);
+ }
 
-
-        }
-
-public void login() {
-    open("http://automationpractice.com/index.php");
-    $(By.className("login")).click();
-    $(By.id("email")).setValue("test1234567@gmail.com");
-    $(By.id("passwd")).click();
-    $(By.id("email")).shouldHave(Condition.cssValue("color","rgba(53, 179, 63, 1)"));
-    sleep(3000);
-    $(By.id("passwd")).setValue("12345");
-    $(By.name("SubmitLogin")).click();
-    $(By.className("info-account")).shouldBe(Condition.text("Welcome to your account. Here you can manage all of your personal information and orders."));
-    sleep(3000);
-
-
-
-
+    public void login() {
+        open("http://automationpractice.com/index.php");
+        $(By.cssSelector("a[href*=my-account]")).click();
+        sleep(3000);
+        $(By.id("email")).setValue(email);
+        $(By.id("passwd")).click();
+        $(By.id("email")).shouldHave(Condition.cssValue("color","rgba(53, 179, 63, 1)"));
+        sleep(3000);
+        $(By.id("passwd")).setValue("12345");
+        $(By.name("SubmitLogin")).click();
+        $(By.className("info-account")).shouldBe(Condition.text("Welcome to your account. Here you can manage all of your personal information and orders."));
+        sleep(3000);
 }
-
     }
--- src/test/java/selenide/hometask/one/hometask/one.java
+++
@@ -1,9 +0,0 @@
-package selenide.hometask.one.hometask;
-
-import org.junit.Test;
-
-public class one {
-    @Test
-    public void registrationFlow() {
-    }
-}
